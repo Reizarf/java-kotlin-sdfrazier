e154c8a8447bd5246f6681f1d7a0b38c81dc725b
Reizarf <sullyfrazier@gmail.com>
2023-04-26 13:41:31 -0600

    Final

--- AgeCalculatorApp/app/src/main/java/com/example/agecalculatorapp/MainActivity.java
+++ AgeCalculatorApp/app/src/main/java/com/example/agecalculatorapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.resources.TextAppearance;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.EditTextID);
         int birthYear = Integer.parseInt(editText.getText().toString());
         int age = 2023 - birthYear;
+        int days = age*365;
         Toast.makeText(this,"Your current age = "+String.valueOf(age),Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(this,"And In days ="+String.valueOf(days),Toast.LENGTH_LONG).show();
     }
 }
